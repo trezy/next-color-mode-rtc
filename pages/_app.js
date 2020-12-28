26e50c6e030c9c7ea0b23070ecc691bd9c691422
Trezy <tre@trezy.com>
2020-12-28 16:26:58 -0600

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import { ColorModeContextProvider } from 'next-color-mode'
+
+export default function App({ Component, pageProps }) {
+  console.log({ColorModeContextProvider})
+  return (
+    <ColorModeContextProvider>
+      <Component {...pageProps} />
+    </ColorModeContextProvider>
+  )
+}
